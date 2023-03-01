e45cc6fc322046c5742cd0998e5642ce915b2a08
Cole Beezley <cdbbeezley@gmail.com>
2023-02-28 22:10:04 -0800

    updated details

--- src/App.js
+++ src/App.js
@@ -4,15 +4,15 @@ function App() {
   return (
     <>
       <nav class="navbar navbar-dark bg-dark navbar-expand-md">
-        <a href="#" class="navbar-brand">paper/cuts</a>
+        <a href="#" class="navbar-brand">paper/cut</a>
         <button class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbar">
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="navbar-collapse collapse" id="navbar">
           <ul class="navbar-nav">
             <li class="nav-item"><a href="#" class="nav-link">Home</a></li>
-            <li class="nav-item"><a href="#" class="nav-link">About</a></li>
-            <li class="nav-item"><a href="#" class="nav-link">Contact</a></li>
+            <li class="nav-item"><a href="#about" class="nav-link">About</a></li>
+            <li class="nav-item"><a href="#contact" class="nav-link">Contact</a></li>
           </ul>
         </div>
       </nav>
